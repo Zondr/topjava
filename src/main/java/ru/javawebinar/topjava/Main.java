b14a5a0df0c3c8197e3d37a1c9c7eef2c70775f7
Evgeny Alelekov <zondr7@gmail.com>
2018-02-25 11:17:58 +0700

    добавил патч

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,4 +1,4 @@
-package ru.javawebinar.topjava;
+//package ru.javawebinar.topjava;
 
 /**
  * User: gkislin
@@ -7,8 +7,9 @@ package ru.javawebinar.topjava;
  * @link http://caloriesmng.herokuapp.com/
  * @link https://github.com/JavaOPs/topjava
  */
-public class Main {
+/*public class Main {
     public static void main(String[] args) {
         System.out.format("Hello Topjava Enterprise!");
     }
 }
+*/
