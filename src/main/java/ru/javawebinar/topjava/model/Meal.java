1c715dac21831e94096d17bb216d0787a0a7ea47
Evgeny Alelekov <zondr7@gmail.com>
2018-03-15 12:22:45 +0700

    сделать Meal extends AbstractBaseEntity

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -4,21 +4,26 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class Meal {
+public class Meal extends AbstractBaseEntity{
     private Integer id;
 
-    private final LocalDateTime dateTime;
+    private LocalDateTime dateTime;
 
-    private final String description;
+    private String description;
 
-    private final int calories;
+    private int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
-    }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        this.id = id;
+
+   // public Meal(LocalDateTime dateTime, String description, int calories) {
+     //   this( dateTime, description, calories);
+    //}
+
+
+
+    public Meal(Integer id, Integer id1, LocalDateTime dateTime, String description, int calories) {
+        super(id);
+        this.id = id1;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
